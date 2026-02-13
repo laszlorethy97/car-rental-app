663aa7c3a1af40ce93b9acc84887c0749a00146a
lrethy <laszlorethy97@gmail.com>
2026-02-13 15:22:18 +0100

    Initial commit: car-rental- project

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CarRentalSystem
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
