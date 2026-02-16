b14805d951aa3bde250ec6ab7112ec72bcf60afd
lrethy <laszlorethy97@gmail.com>
2026-02-16 19:17:19 +0100

    add-dbsets

--- DatabaseConfig/CarRentalDbContext.cs
+++ DatabaseConfig/CarRentalDbContext.cs
@@ -6,14 +6,9 @@ namespace FishingShop
         public DbSet<Rental> Rentals { get; set; } = null!;
         public DbSet<Car> Cars { get; set; } = null!;
         public DbSet<Invoince> Invoinces { get; set; } = null!;
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=FishingShopDb;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True;");
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
         }
     }
 }
