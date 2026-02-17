ff55419d7bec95515007b8462675d0313ae16393
lrethy <laszlorethy97@gmail.com>
2026-02-17 10:26:29 +0100

    Enums moved to dedicated .cs files

--- DatabaseConfig/CarRentalDbContext.cs
+++ DatabaseConfig/CarRentalDbContext.cs
@@ -1,20 +1,19 @@
 using Microsoft.EntityFrameworkCore;
-namespace CarRentalSystem
-{
-    public class CarRentalDbContext : DbContext
-    {
-        public DbSet<User> Users { get; set; } = null!;
-        public DbSet<Rental> Rentals { get; set; } = null!;
-        public DbSet<Car> Cars { get; set; } = null!;
-        public DbSet<Invoice> Invoices { get; set; } = null!;
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=CarRentalDb;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True;");
-        }
+namespace CarRentalSystem;
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            
-        }
+public class CarRentalDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+    public DbSet<Rental> Rentals { get; set; } = null!;
+    public DbSet<Car> Cars { get; set; } = null!;
+    public DbSet<Invoice> Invoices { get; set; } = null!;
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=CarRentalDb;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
     }
 }
