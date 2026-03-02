c495809bc80cf0bea9e489f7b9b5c69315c0c67e
lrethy <laszlorethy97@gmail.com>
2026-03-02 16:59:46 +0100

    create endpoint get/post/put to invoices

--- Controllers/Controller.cs
+++ Controllers/Controller.cs
@@ -70,5 +70,30 @@ public class CarRentalController: ControllerBase
     {
         await manager.UpdateUser(id, user);
     }
+
+
+   [HttpGet("invoices", Name = "GetInvoices")]
+    async public Task<List<Invoice>> GetInvoice()
+    {
+        return await manager.GetInvoice();
+    }
+
+    [HttpGet("invoice/{id}", Name = "GetInvoice")]
+    async public Task<Invoice?> GetInvoiceById(int id)
+    {
+        return await manager.GetInvoiceById(id);
+    }
+
+    [HttpPost("invoice", Name = "AddInvoice")]
+    async public Task AddInvoice(Invoice invoice)
+    {
+        await manager.AddInvoice(invoice);
+    }
+
+    [HttpPut("invoice/{id}", Name = "UpdateInvoice")]
+    public async Task UpdateInvoice(int id, [FromBody] Invoice invoice)
+    {
+        await manager.UpdateInvoice(id, invoice);
+    }
 }
 
--- Managgers/Managger.cs
+++ Managgers/Managger.cs
@@ -73,4 +73,29 @@ public class Managger
         context.Users.Update(user);
         await context.SaveChangesAsync();
     }
+
+
+    public async Task<List<Invoice>> GetInvoice()
+    {
+        return await context.Invoices.ToListAsync();
+    }
+
+    public async Task<Invoice?> GetInvoiceById(int id)
+    {
+        return await context.Invoices.FirstOrDefaultAsync(c => c.Id == id);
+    }
+
+    public async Task AddInvoice(Invoice invoice)
+    {
+        await context.Invoices.AddAsync(invoice);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task UpdateInvoice(int id, Invoice invoice)
+    {
+        invoice.Id = id;
+        context.Invoices.Update(invoice);
+        await context.SaveChangesAsync();
+    }
+
 }
