f98fc4ae165b2855a845067d1d406a99587ef580
lrethy <laszlorethy97@gmail.com>
2026-03-02 16:24:13 +0100

    create users endpoints get/post/put

--- Controllers/Controller.cs
+++ Controllers/Controller.cs
@@ -45,5 +45,30 @@ public class CarRentalController: ControllerBase
     {
         await manager.UpdateCarByid(id, car);
     }*/
+
+
+    [HttpGet("users", Name = "GetUsers")]
+    async public Task<List<User>> GetUsers()
+    {
+        return await manager.GetUsers();
+    }
+
+    [HttpGet("user/{id}", Name = "GetUser")]
+    async public Task<User?> GetUserById(int id)
+    {
+        return await manager.GetUserById(id);
+    }
+
+    [HttpPost("user", Name = "AddUser")]
+    async public Task AddUser(User user)
+    {
+        await manager.AddUser(user);
+    }
+
+    [HttpPut("user/{id}", Name = "Updateuser")]
+    public async Task UpdateUser(int id, [FromBody] User user)
+    {
+        await manager.UpdateUser(id, user);
+    }
 }
 
--- Managgers/Managger.cs
+++ Managgers/Managger.cs
@@ -51,4 +51,26 @@ public class Managger
         if(newData.Year != null) oldData.Year = newData.Year;
         return oldData;        
     }*/
+    public async Task<List<User>> GetUsers()
+    {
+        return await context.Users.Include(user => user.Roles).ToListAsync();
+    }
+
+    public async Task<User?> GetUserById(int id)
+    {
+        return await context.Users.Include(user => user.Roles).FirstOrDefaultAsync(c => c.Id == id);
+    }
+
+    public async Task AddUser(User user)
+    {
+        await context.Users.AddAsync(user);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task UpdateUser(int id, User user)
+    {
+        user.Id = id;
+        context.Users.Update(user);
+        await context.SaveChangesAsync();
+    }
 }
