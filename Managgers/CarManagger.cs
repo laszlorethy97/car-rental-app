4c2f200e59ba850b9aa3efcf9b456d0fc520b515
lrethy <laszlorethy97@gmail.com>
2026-03-04 10:11:12 +0100

    make carManagger

--- Managgers/CarManagger.cs
+++ Managgers/CarManagger.cs
@@ -9,6 +9,26 @@ public class CarManagger
     {
         this.context = context;
     }
+    
+    public async Task<List<Car>> GetCars()
+    {
+        return await context.Cars.ToListAsync();
+    }
+    public async Task<Car?> GetCarById(int id)
+    {
+        return await context.Cars.FirstOrDefaultAsync(c => c.Id == id);
+    }
+    public async Task AddCar(Car car)
+    {
+        await context.Cars.AddAsync(car);
+        await context.SaveChangesAsync();
+    }
+    public async Task UpdateCar(int id, Car car)
+    {
+        car.Id = id;
+        context.Cars.Update(car);
+        await context.SaveChangesAsync();
+    }
 
     
 }
