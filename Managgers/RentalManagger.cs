aae929c99a5d29098546a1067ad2fef3fb8f246e
lrethy <laszlorethy97@gmail.com>
2026-03-04 10:15:58 +0100

    make RentalManagger

--- Managgers/RentalManagger.cs
+++ Managgers/RentalManagger.cs
@@ -9,4 +9,26 @@ public class RentalManagger
     {
         this.context = context;
     }
+
+    public async Task<List<Rental>> GetRentals()
+    {
+        return await context.Rentals.ToListAsync();
+    }
+
+    public async Task<Rental?> GetRentalById(int id)
+    {
+        return await context.Rentals.FirstOrDefaultAsync(r => r.Id == id);
+    }
+    
+    public async Task AddRental(Rental rental)
+    {
+        await context.Rentals.AddAsync(rental);
+        await context.SaveChangesAsync();
+    }
+    public async Task UpdateRentalByid(int id, Rental rental)
+    {
+        rental.Id = id;
+        context.Rentals.Update(rental);
+        await context.SaveChangesAsync();
+    }
 }
