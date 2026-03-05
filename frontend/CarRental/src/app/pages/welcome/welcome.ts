e9c25f2d82c3a9b796835ad48dcf6d5c51c2b3b5
lrethy <laszlorethy97@gmail.com>
2026-03-05 17:49:53 +0100

    create routing from welcomecomponent

--- frontend/CarRental/src/app/pages/welcome/welcome.ts
+++ frontend/CarRental/src/app/pages/welcome/welcome.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-welcome',
@@ -6,4 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './welcome.html',
   styleUrl: './welcome.scss',
 })
-export class Welcome {}
+export class Welcome {
+
+  constructor(private readonly router: Router){}
+
+  navigateToRegisterComponent(){
+    this.router.navigate(['register']);
+  }
+
+  navigateToLoginComponent(){
+    this.router.navigate(['login']);
+  }
+
+  navigateToGuesAccessComponent(){
+    this.router.navigate(['guest-access']);
+  }
+}
