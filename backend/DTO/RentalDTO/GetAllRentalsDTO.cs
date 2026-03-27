bd16468eab34d182e097436176bef65791db59ae
VDani <vorosdaniel1214@gmail.com>
2026-03-27 15:58:06 +0100

    Added-GetAllRentals-endpoint-and-DTO

---
+++ backend/DTO/RentalDTO/GetAllRentalsDTO.cs
@@ -0,0 +1,13 @@
+namespace CarRentalSystem;
+
+public class GetAllRentalsDTO
+{
+    public int RentalId { get; set; }
+    public string LicensePlate {get; set;}
+    public string Brand {get; set;}
+    public string Model {get; set;}
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string RentStatus { get; set; }
+    public int RentPrice {get; set;}
+}
