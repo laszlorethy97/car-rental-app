457beeac13e263172416f6cef67af7ab4b45e394
lrethy <laszlorethy97@gmail.com>
2026-04-03 12:19:43 +0200

    add new features

---
+++ frontend/CarRental/src/app/models/guest-rent-car-dto.ts
@@ -0,0 +1,10 @@
+export interface GuestRentCarDto {
+    carId: number;
+    email: string;
+    firstName: string;
+    lastName: string;
+    addres: string;
+    telephon: string;
+    startDate: string;
+    endDate: string;
+}
